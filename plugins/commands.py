3c0b5223f5df95bae0373710448c0db247d0808c
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-03-07 19:20:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,7 +57,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton("ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ : ʀᴇᴍᴏᴠᴇ ᴀᴅs", url="https://tools.apgy.in/upi/SvFilmsX/BHARATPE09917110955@yesbankltd/")
         ],[
-            InlineKeyboardButton("ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ : ʀᴇᴍᴏᴠᴇ ᴀᴅs", callback_data="my_plans")
+            InlineKeyboardButton("ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ : ʀᴇᴍᴏᴠᴇ ᴀᴅs", url="https://tools.apgy.in/upi/SvFilmsX/BHARATPE09917110955@yesbankltd/")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -1173,7 +1173,7 @@ async def plans_cmd_handler(client, message):
     reply_markup = InlineKeyboardMarkup(btn)
     await message.reply_photo(
         photo="https://telegra.ph/file/0c496e7f82cd7619f74f5.jpg",
-        caption="**Pʀᴇᴍɪᴜᴍ Fᴇᴀᴛᴜʀᴇs 🎁\n\n☆ No Need To Verify\n☆ Ad Free Experience\n☆ Unlimited Movie And Series\n\n☆ Plan Detain 👇\n\n☆  10₹ - 1 day ( Trail )\n☆  30₹ - 1 week\n☆  70₹ - 1 month\n☆  350₹ - 6 months\n☆  600₹ - 1 year",
+        caption="**Pʀᴇᴍɪᴜᴍ Fᴇᴀᴛᴜʀᴇs 🎁\n\n☆ No Need To Verify\n☆ Ad Free Experience\n☆ Unlimited Movie And Series\n\n☆ Plan Detail 👇\n\n☆  10₹ - 1 day ( Trail )\n☆  30₹ - 1 week\n☆  70₹ - 1 month\n☆  350₹ - 6 months\n☆  600₹ - 1 year",
         reply_markup=reply_markup
     )
         
